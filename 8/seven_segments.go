36764493077be25476a40e5ceae1a8c94af348bd
Will Munn <will.munn@focusrite.com>
2021-12-30 07:31:10 +0000

    Run go fmt

--- 8/seven_segments.go
+++ 8/seven_segments.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "io/ioutil"
-    "log"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
 )
 
 type digit struct {
-    SegmentCount int
-    Value int
+	SegmentCount int
+	Value        int
 }
 
 var digits = []digit{
-    digit{2, 1},
-    digit{4, 4},
-    digit{3, 7},
-    digit{7, 8},
+	digit{2, 1},
+	digit{4, 4},
+	digit{3, 7},
+	digit{7, 8},
 }
 
 func Parse(input string) []digit {
-    lines := strings.Split(input, "\n")
-    found := make([]digit, 0, 4)
-    for _, line := range(lines) {
-        parts := strings.Split(line, "|")
-        if (len(parts) < 2) {
-            continue
-        }
-        segmentsGroups := strings.Split(strings.Trim(parts[1], " "), " ")
-        for _, group := range segmentsGroups {
-            for _, digit := range digits {
-                if len(group) == digit.SegmentCount {
-                    found = append(found, digit)
-                }
-            }
-        }
-    }
-    return found
+	lines := strings.Split(input, "\n")
+	found := make([]digit, 0, 4)
+	for _, line := range lines {
+		parts := strings.Split(line, "|")
+		if len(parts) < 2 {
+			continue
+		}
+		segmentsGroups := strings.Split(strings.Trim(parts[1], " "), " ")
+		for _, group := range segmentsGroups {
+			for _, digit := range digits {
+				if len(group) == digit.SegmentCount {
+					found = append(found, digit)
+				}
+			}
+		}
+	}
+	return found
 }
 
 func main() {
-    content, err := ioutil.ReadFile("./input.txt")
-        if err != nil {
-        log.Fatal(err)
-    }
-    input := string(content)
-    fmt.Println(len(Parse(input)))
+	content, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	input := string(content)
+	fmt.Println(len(Parse(input)))
 }
--- 8/seven_segments_test.go
+++ 8/seven_segments_test.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestPart1(t *testing.T) {
-    input := "fio | fdgacbe cefdb cefbgd gcbe\nfio | fdgacbe cefdb cefbgd gcbe"
-    parsed := Parse(input)
-    if parsed[0].SegmentCount != 7 {
-        t.Errorf("expected 7 got %d", parsed[0].SegmentCount)
-    }
-    if (len(parsed) != 4) {
-        t.Errorf("Expected length 4 got %d", len(parsed))
-    }
+	input := "fio | fdgacbe cefdb cefbgd gcbe\nfio | fdgacbe cefdb cefbgd gcbe"
+	parsed := Parse(input)
+	if parsed[0].SegmentCount != 7 {
+		t.Errorf("expected 7 got %d", parsed[0].SegmentCount)
+	}
+	if len(parsed) != 4 {
+		t.Errorf("Expected length 4 got %d", len(parsed))
+	}
 }
