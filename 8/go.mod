fe0cf6b03b78487a2c7a26db2ab109e8f20424f8
Will Munn <will.munn@focusrite.com>
2021-12-30 07:25:44 +0000

    Problem 8 part 1

---
+++ 8/go.mod
@@ -0,0 +1,3 @@
+module aoc/8
+
+go 1.13
---
+++ 8/seven_segments.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+    "fmt"
+    "strings"
+    "io/ioutil"
+    "log"
+)
+
+type digit struct {
+    SegmentCount int
+    Value int
+}
+
+var digits = []digit{
+    digit{2, 1},
+    digit{4, 4},
+    digit{3, 7},
+    digit{7, 8},
+}
+
+func Parse(input string) []digit {
+    lines := strings.Split(input, "\n")
+    found := make([]digit, 0, 4)
+    for _, line := range(lines) {
+        parts := strings.Split(line, "|")
+        if (len(parts) < 2) {
+            continue
+        }
+        segmentsGroups := strings.Split(strings.Trim(parts[1], " "), " ")
+        for _, group := range segmentsGroups {
+            for _, digit := range digits {
+                if len(group) == digit.SegmentCount {
+                    found = append(found, digit)
+                }
+            }
+        }
+    }
+    return found
+}
+
+func main() {
+    content, err := ioutil.ReadFile("./input.txt")
+        if err != nil {
+        log.Fatal(err)
+    }
+    input := string(content)
+    fmt.Println(len(Parse(input)))
+}
