041a5752267eb8ae2ac35ae6f49a02b77fe2ff5b
Will Munn <will.munn@focusrite.com>
2022-01-09 11:40:34 +0000

    WIP rework digit parsing by looking at signal patterns

--- 8/seven_segments.go
+++ 8/seven_segments.go
@@ -4,37 +4,93 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-type digit struct {
+type Digit struct {
 	SegmentCount int
 	Value        int
 }
 
-var digits = []digit{
-	digit{2, 1},
-	digit{4, 4},
-	digit{3, 7},
-	digit{7, 8},
+var digits = []Digit{
+	{6, 0}, //
+	{2, 1},
+	{5, 2}, //
+	{5, 3}, //
+	{4, 4},
+	{5, 5}, //
+	{6, 6}, //
+	{3, 7},
+	{7, 8},
+	{6, 9}, //
+
+	// 6 segments 0, 6, 9
 }
 
-func Parse(input string) []digit {
-	lines := strings.Split(input, "\n")
-	found := make([]digit, 0, 4)
-	for _, line := range lines {
-		parts := strings.Split(line, "|")
-		if len(parts) < 2 {
-			continue
+func find(digits []Digit, predicate func(Digit) bool) Digit {
+	for _, digit := range digits {
+		if predicate(digit) {
+			return digit
 		}
-		segmentsGroups := strings.Split(strings.Trim(parts[1], " "), " ")
-		for _, group := range segmentsGroups {
-			for _, digit := range digits {
-				if len(group) == digit.SegmentCount {
-					found = append(found, digit)
-				}
+	}
+	return Digit{}
+}
+
+func contains(numbers []int, needle int) bool {
+	for _, number := range numbers {
+		if number == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func sortString(in string) string {
+	groupAsSlice := strings.Split(in, "")
+	sort.Slice(groupAsSlice, func(a, b int) bool {
+		return groupAsSlice[a] < groupAsSlice[b]
+	})
+	return strings.Join(groupAsSlice, "")
+}
+
+func ParseMappings(input []string) map[string]int {
+	mappings := make(map[string]int)
+	for _, wires := range input {
+		if contains([]int{2, 3, 4, 7}, len(wires)) {
+			withLength := func(d Digit) bool {
+				return d.SegmentCount == len(wires)
 			}
+			mappings[sortString(wires)] = find(digits, withLength).Value
 		}
+	}
+	return mappings
+}
+
+func ParseLine(line string) string {
+	result := ""
+	parts := strings.Split(line, "|")
+	if len(parts) < 2 {
+		return ""
+		// should error
+	}
+	mappings := ParseMappings(strings.Split(parts[0], " "))
+	outputSegments := strings.Split(strings.Trim(parts[1], " "), " ")
+	for _, segments := range outputSegments {
+		i, ok := mappings[sortString(segments)]
+		if ok {
+			result = result + strconv.Itoa(i)
+		}
+	}
+	return result
+}
+
+func Parse(input string) []string {
+	lines := strings.Split(input, "\n")
+	found := make([]string, 0, len(lines))
+	for _, line := range lines {
+		found = append(found, ParseLine(line))
 	}
 	return found
 }
--- 8/seven_segments_test.go
+++ 8/seven_segments_test.go
@@ -1,16 +1,77 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestPart1(t *testing.T) {
-	input := "fio | fdgacbe cefdb cefbgd gcbe\nfio | fdgacbe cefdb cefbgd gcbe"
-	parsed := Parse(input)
-	if parsed[0].SegmentCount != 7 {
-		t.Errorf("expected 7 got %d", parsed[0].SegmentCount)
+/*func TestOneLine(t *testing.T) {
+	input := [4]string{"fdgacbe", "cefdb", "cefbgd", "gcbe"}
+	output := GetLineOutput(input)
+
+	if output != "8394" {
+		t.Errorf("Wrong output value %s expected %s", output, "8394")
 	}
-	if len(parsed) != 4 {
+}*/
+
+/*func GetLineOutput(input [4]string) string {
+	found := make([]string, 0, 4)
+	for _, group := range input {
+		groupAsSlice := strings.Split(group, "")
+		sort.Slice(groupAsSlice, func(a, b int) bool {
+			return groupAsSlice[a] < groupAsSlice[b]
+		})
+		for _, digit := range digits {
+			if strings.Join(groupAsSlice, "") == digit.Segments {
+				found = append(found, strconv.Itoa(digit.Value))
+			}
+		}
+	}
+	return strings.Join(found, "")
+}*/
+
+func TestParseMappings(t *testing.T) {
+	input := []string{"acedgfb", "cdfbe", "gcdfa", "fbcad", "dab", "cefabd", "cdfgeb", "eafb", "cagedb", "ab"}
+	mappings := ParseMappings(input)
+
+	if mappings["abcdefg"] != 8 {
+		t.Errorf("abcdefg should equal 8 was %d", mappings["abcdefg"])
+	}
+
+	if mappings["abd"] != 7 {
+		t.Errorf("abd should equal 7 was %d", mappings["abd"])
+	}
+
+	if mappings["abef"] != 4 {
+		t.Errorf("abd should equal 4 was %d", mappings["abef"])
+	}
+
+	if mappings["ab"] != 1 {
+		t.Errorf("abd should equal 1 was %d", mappings["ab"])
+	}
+
+	if mappings["bcdef"] != 5 {
+		t.Errorf("abd should equal 5 was %d", mappings["bcdef"])
+	}
+}
+
+func TestParseLine(t *testing.T) {
+	line := "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe"
+	output := ParseLine(line)
+	if output != "84" {
+		t.Errorf("expected ouput to be 84 was %s", output)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	input := "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe\nedbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fdgacbe cefdb cefbgd gcbe"
+	parsed := Parse(input)
+
+	if parsed[0] != "84" {
+		t.Errorf("expected 84 got %s", parsed[0])
+	}
+	fmt.Printf("%s", parsed)
+	if len(parsed) != 2 {
 		t.Errorf("Expected length 4 got %d", len(parsed))
 	}
 }
