d1630bbdd42fd4d6a2d43a9585ce4ed63acea494
Will Munn <william.munn@rubix.com>
2023-12-09 15:53:59 +0100

    Day 8 with some help

--- 2023/eight/src/main.rs
+++ 2023/eight/src/main.rs
@@ -1,10 +1,17 @@
+use num::integer::lcm;
 use std::fs;
 
 fn main() {
     let contents =
         fs::read_to_string("./input.txt").expect("Should have been able to read the file");
     let steps = steps_to_zzz(&contents);
-    println!("part 1 {}", steps);
+    //let part_2 = steps_to_zzz_ghosts(&contents);
+    let a: i64 = lcm(11911, 13019);
+    let b = lcm(16343, 19667);
+    let c = lcm(20221, 21883);
+    let d = lcm(a, b);
+    let part_2 = lcm(d, c);
+    println!("part 1 {} part 2 {}", steps, part_2);
 }
 
 #[derive(Debug)]
@@ -14,7 +21,7 @@ struct Place<'a> {
     right: &'a str,
 }
 
-fn steps_to_zzz(input: &str) -> usize {
+fn parse(input: &str) -> (Vec<char>, Vec<Place>) {
     let parts: Vec<&str> = input.split("\n\n").collect();
     let directions: Vec<char> = parts[0].chars().collect();
     let places: Vec<Place> = parts[1]
@@ -26,6 +33,45 @@ fn steps_to_zzz(input: &str) -> usize {
             right: &line[12..15],
         })
         .collect();
+    (directions, places)
+}
+
+fn steps_to_zzz_ghosts(input: &str) -> usize {
+    let (directions, places) = parse(input);
+    let mut steps: usize = 0;
+    let mut positions: Vec<&Place> = places.iter().filter(|p| p.name.ends_with("A")).collect();
+
+    while !positions.iter().all(|&p| p.name.ends_with("Z")) {
+        let zspots: Vec<&&Place> = positions
+            .iter()
+            .filter(|&p| p.name.ends_with("Z"))
+            .collect();
+        if zspots.len() > 0 {
+            println!("{:?} {}", zspots, steps);
+        }
+        let direction = directions[steps % directions.len()];
+        steps = steps + 1;
+        positions = positions
+            .iter()
+            .map(|place| {
+                if direction == 'L' {
+                    return places
+                        .iter()
+                        .find(|p| p.name.eq(place.left))
+                        .expect("failed to find place");
+                }
+                if direction == 'R' {
+                    return places.iter().find(|p| p.name.eq(place.right)).unwrap();
+                }
+                panic!("should never happen");
+            })
+            .collect();
+    }
+    steps
+}
+
+fn steps_to_zzz(input: &str) -> usize {
+    let (directions, places) = parse(input);
 
     let mut steps: usize = 0;
     let mut place = places.first().expect("");
@@ -59,4 +105,20 @@ ZZZ = (ZZZ, ZZZ)"#;
 
     let steps = steps_to_zzz(input);
     assert_eq!(steps, 2);
+}
+
+#[test]
+fn example_2_works() {
+    let input = r#"LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"#;
+    let steps = steps_to_zzz_ghosts(input);
+    assert_eq!(steps, 6);
 }
